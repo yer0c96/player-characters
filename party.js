d954a5ebf92b324ddd3e84c6a977d89293754910
Richard Jarrell <richardjarrell@yahoo.com>
2023-01-24 23:55:23 -0600

    simplified inventory list

--- party.js
+++ party.js
@@ -1,6 +1,16 @@
 const fs = require('fs')
 const { sum, pluck, pipe, flatten, sort, sortBy, prop, identity } = require('ramda')
 
+const ignoredItems = [
+  'Backpack',
+  'Stress I',
+  'Stress II',
+  'Stress III',
+  'Stress IV',
+  'Stress V',
+  'Emotional Baggage',
+]
+
 const partySummary = () => {
   const josh = JSON.parse(fs.readFileSync('summary/josh.json'))
   const corey = JSON.parse(fs.readFileSync('summary/corey.json'))
@@ -10,10 +20,22 @@ const partySummary = () => {
 
   const party = [josh, corey, todd, gamel, jen]
 
+  const inventories = pipe(pluck('inventory'), flatten, sortBy(identity))(party)
+
+  const inventory = inventories
+    .reduce((a, b) => {
+      const index = a.findIndex((item) => item.name === b)
+      return index === -1 ? a.push({ name: b, count: 1 }) : a[index].count++, a
+    }, [])
+    .filter((item) => !ignoredItems.includes(item.name))
+    .map((item) => (item.count > 1 ? `${item.name} x${item.count}` : item.name))
+
+  console.log(inventory)
+
   const summary = {
     classes: pipe(pluck('classes'))(party),
     money: sum(pluck('money', party)),
-    inventory: pipe(pluck('inventory'), flatten, sortBy(identity))(party),
+    inventory,
   }
 
   fs.writeFileSync(`summary/party.json`, JSON.stringify(summary))
